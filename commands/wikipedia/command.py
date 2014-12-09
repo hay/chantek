19775383f3d0c1364e60cf02e229910e0a17723e
Hay Kranen <huskyr@gmail.com>
2014-12-09 15:28:41 +0100

    Expanded the wikipedia command

--- commands/wikipedia/command.py
+++ commands/wikipedia/command.py
@@ -1,6 +1,14 @@
 import wikipedia
 
-methods = ("define", "suggest", "pageviews", "linkshere", "langlinks", "statistics")
+methods = (
+    "article",
+    "define",
+    "suggest",
+    "pageviews",
+    "linkshere",
+    "langlinks",
+    "statistics"
+)
 
 def run(args, method):
     if "q" not in args:
@@ -8,28 +16,35 @@ def run(args, method):
 
     opts = {
         "lang" : args.get("lang", "en"),
+        "imgwidth" : args.get("imgwidth", 500),
         "q" : args["q"]
     }
 
+    if method == "article":
+        return wikipedia.article(**opts)
+
+    if method == "imgresize":
+        return wikipedia.imgresize(**opts)
+
     if method == "define":
-       return wikipedia.define(opts)
+       return wikipedia.define(**opts)
 
     if method == "suggest":
-        return wikipedia.suggest(opts)
+        return wikipedia.suggest(**opts)
 
     if method == "pageviews":
-        return wikipedia.pageviews(opts)
+        return wikipedia.pageviews(**opts)
 
     if method == "linkshere":
-        return wikipedia.linkshere(opts)
+        return wikipedia.linkshere(**opts)
 
     if method == "langlinks":
-        return wikipedia.langlinks(opts)
+        return wikipedia.langlinks(**opts)
 
     if method == "statistics":
-        langlinks = wikipedia.langlinks(opts)
-        linkshere = wikipedia.linkshere(opts)
-        pageviews = wikipedia.pageviews(opts)
+        langlinks = wikipedia.langlinks(**opts)
+        linkshere = wikipedia.linkshere(**opts)
+        pageviews = wikipedia.pageviews(**opts)
 
         return {
             "langlinks" : langlinks["total"],
--- commands/wikipedia/wikipedia.py
+++ commands/wikipedia/wikipedia.py
@@ -1,6 +1,7 @@
 import requests, json, re, time
 
 API_ENDPOINT = "http://%s.wikipedia.org/w/api.php";
+RESIZE_ENDPOINT = "http://commons.wikimedia.org/wiki/Special:Redirect/file/%s?width=%s"
 
 def request(lang, params):
     url = API_ENDPOINT % lang
@@ -11,34 +12,126 @@ def request(lang, params):
 
     return data
 
-def define(args):
-    q = args["q"]
-    lang = args["lang"] if "lang" in args else "en"
-
-    data = request(lang, {
-        "action" : "query",
-        "prop" : "extracts",
-        "exintro" : 1,
-        "titles" : q
-    })
+def _getfirstpage(data):
+    if "-1" in data["query"]["pages"]:
+        return False
 
     # Sigh.. awful Wikipedia API crap
     pageid = data['query']['pages'].keys()[0]
+    return data["query"]["pages"][pageid]
 
-    if "-1" in data["query"]["pages"]:
+def _extracts(q, lang, intro = True):
+    opts = {
+        "action" : "query",
+        "prop" : "extracts",
+        "titles" : q
+    }
+
+    if intro:
+        opts["exintro"] = 1
+
+    data = request(lang, opts)
+
+    page = _getfirstpage(data)
+
+    if not page:
         return False
-    else :
-        page = data["query"]["pages"][pageid]
 
+    if intro:
         # A bit ugly, but oh well
         page['extract'] = re.sub('<[^<]+?>', '', page['extract']).strip()
 
-        return page
+    return page
 
-def suggest(args):
-    q = args["q"]
-    lang = args["lang"] if "lang" in args else "en"
+def imgresize(q, lang, imgwidth):
+    # We got to remove the Namespace prefix, in all languages :/
+    if ":" in q:
+        parts = q.split(":")
+        q = parts[1]
 
+    return RESIZE_ENDPOINT % (q, imgwidth)
+
+def article(q, lang, imgwidth):
+    text = _extracts(q, lang, False)
+
+    if not text:
+        return False
+
+    text = text["extract"]
+    images = articleimages(q, lang)
+    images = imageinfo(images, lang)
+
+    # Remove all 'local' images, we can't redirect those to Commons
+    images = filter(lambda i:i["imagerepository"] == "shared", images)
+
+    for img in images:
+        img["thumb"] = imgresize(img["title"], lang, imgwidth)
+
+    return {
+        "text" : text,
+        "thumbnail" : thumbnail(q, lang, imgwidth),
+        "images" : images
+    }
+
+def _imageinfo_format(img):
+    if "imageinfo" not in img:
+        return False
+
+    info = img["imageinfo"][0]
+
+    info.update({
+        "imagerepository" : img["imagerepository"],
+        "title" : img["title"]
+    })
+
+    return info
+
+def thumbnail(q, lang, imgwidth):
+    data = request(lang, {
+        "action" : "query",
+        "prop" : "pageimages",
+        "titles" : q,
+        "pithumbsize" : imgwidth
+    })
+
+    data = _getfirstpage(data)
+
+    if not data:
+        return False
+
+    # Consistentcy, people!
+    data["thumbnail"]["url"] = data["thumbnail"]["source"]
+
+    return data["thumbnail"]
+
+def imageinfo(images, lang):
+    data = request(lang, {
+        "action" : "query",
+        "prop" : "imageinfo",
+        "titles" : "|".join(images),
+        "iiprop" : "url"
+    })
+
+    return map(_imageinfo_format, data["query"]["pages"].values())
+
+def articleimages(q, lang):
+    data = request(lang, {
+        "action" : "query",
+        "prop" : "images",
+        "titles" : q
+    })
+
+    page = _getfirstpage(data)
+
+    if not page:
+        return False
+
+    return map(lambda x:x["title"], page["images"])
+
+def define(q, lang):
+    return _extracts(q, lang, True)
+
+def suggest(q, lang):
     data = request(lang, {
         "action" : "opensearch",
         "search" : q,
@@ -51,10 +144,7 @@ def suggest(args):
     }
 
 # Right now, this only returns last month
-def pageviews(args):
-    q = args["q"]
-    lang = args["lang"] if "lang" in args else "en"
-
+def pageviews(q, lang):
     url = "http://stats.grok.se/json/%s/%s/%s" % (
         lang,
         "latest30",
@@ -72,10 +162,7 @@ def pageviews(args):
 
     return stats
 
-def linkshere(args):
-    q = args["q"]
-    lang = args["lang"] if "lang" in args else "en"
-
+def linkshere(q, lang):
     data = request(lang, {
         "action" : "query",
         "prop" : "linkshere",
@@ -92,10 +179,7 @@ def linkshere(args):
 
     return data
 
-def langlinks(args):
-    q = args["q"]
-    lang = args["lang"] if "lang" in args else "en"
-
+def langlinks(q, lang):
     data = request(lang, {
         "action" : "query",
         "prop" : "langlinks",
